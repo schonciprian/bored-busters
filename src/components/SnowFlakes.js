da5e2510755337db86d4e86a640048618a2b4b6f
schonciprian <60438162+schonciprian@users.noreply.github.com>
2020-12-15 20:18:18 +0100

    Add: snowflake divs with snowflake icon

---
+++ src/components/SnowFlakes.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function SnowFlakes() {
+    return (
+        <div class="snowflakes" aria-hidden="true">
+            <div class="snowflake">❅</div>
+            <div class="snowflake">❅</div>
+            <div class="snowflake">❅</div>
+            <div class="snowflake">❅</div>
+            <div class="snowflake">❅</div>
+            <div class="snowflake">❅</div>
+            <div class="snowflake">❅</div>
+            <div class="snowflake">❅</div>
+            <div class="snowflake">❅</div>
+            <div class="snowflake">❅</div>
+            <div class="snowflake">❅</div>
+            <div class="snowflake">❅</div>
+            <div class="snowflake">❅</div>
+            <div class="snowflake">❅</div>
+            <div class="snowflake">❅</div>
+        </div>
+    )
+}
