49eae54b6d5efbf37ac96818e99954f36bde3f54
szabomidu <60429813+szabomidu@users.noreply.github.com>
2020-12-15 11:03:54 +0100

    Changed the style of the give me a random button, added link to the data of the random activity if present

--- src/components/RandomActivity.js
+++ src/components/RandomActivity.js
@@ -1,32 +1,49 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import axios from "axios";
 
-
 export default function Header() {
+  const [randomActivity, setrandomActivity] = useState([]);
 
-    const [randomActivity, setrandomActivity] = useState([]);
+  const getRandomActivity = () => {
+    axios
+      .get("http://www.boredapi.com/api/activity/")
+      .then((response) => setrandomActivity(response.data));
+  };
 
-    const getRandomActivity = () => {
-        axios
-            .get("http://www.boredapi.com/api/activity/")
-            .then((response) => setrandomActivity(response.data))
-    }
+  const getRandomButtonStyle = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    cursor: "pointer",
+    background: "blue",
+    borderRadius: "10px",
+    width: "225px",
+    height: "25px",
+    color: "#fff",
+  };
 
-    const {
-        activity,
-        type,
-        participants,
-        price
-    } = randomActivity;
+  const { activity, type, participants, price } = randomActivity;
 
-    return randomActivity.length !== 0 ? (
+  return randomActivity.length !== 0 ? (
+    <div>
+      <div onClick={getRandomActivity} style={getRandomButtonStyle}>
+        Give me a random activity!
+      </div>
+      <div>{activity}</div>
+      <div>Type: {type}</div>
+      {randomActivity.link ? (
         <div>
-            <div onClick={getRandomActivity}>GETRANDOM</div>
-
-            <div>{activity}</div>
-            <div>Type: {type}</div>
-            <div>Number of participants:{participants}</div>
-            <div>Price: {price * 10000}</div>
+          Visit: <a href={randomActivity.link}> {randomActivity.link}</a>{" "}
         </div>
-    ) : (<div onClick={getRandomActivity}>GETRANDOM</div>);
+      ) : (
+        ""
+      )}
+      <div>Number of participants:{participants}</div>
+      <div>Price: {price * 10000}</div>
+    </div>
+  ) : (
+    <div onClick={getRandomActivity} style={getRandomButtonStyle}>
+      Give me a random activity!
+    </div>
+  );
 }
