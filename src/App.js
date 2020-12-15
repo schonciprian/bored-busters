6b652c16da92de8d5440562189d36a87f442e23d
schonciprian <60438162+schonciprian@users.noreply.github.com>
2020-12-15 10:45:11 +0100

    Added components and routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import Header from './components/Header';
+import Navbar from './components/Navbar';
+import RandomActivity from './components/RandomActivity';
 
 
 function App() {
@@ -8,10 +10,15 @@ function App() {
     <div className="App">
       <Router>
         <Header />
-        <Route path="/" render={(props) => (
+        <Navbar />
+
+        <Route exact path="/" render={(props) => (
           <div>Home page</div>
         )}>
         </Route>
+
+        <Route path="/random" component={RandomActivity}></Route>
+
       </Router>
     </div>
   );
