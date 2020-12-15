9feaee49a65422ef36dc3cd29d906cd9d9853ab1
schonciprian <60438162+schonciprian@users.noreply.github.com>
2020-12-15 10:05:33 +0100

    Added route to main page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import Header from './components/Header';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Router>
+        <Header />
+        <Route path="/" render={(props) => (
+          <div>Home page</div>
+        )}>
+        </Route>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import StyledHeader from "./styledComponents/StyledHeader"
+import { Link } from "react-router-dom"
+
+export default function Header() {
+    return (
+        <StyledHeader>
+            <Link to="/">
+                Bored Busters
+            </Link>
+        </StyledHeader>
+    )
+}
