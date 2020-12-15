f374b7f073f055cb7fd2649dba3f3800d3fa8a8f
schonciprian <60438162+schonciprian@users.noreply.github.com>
2020-12-15 12:02:50 +0100

    Home page rewrite with component, added container div

--- src/App.js
+++ src/App.js
@@ -2,25 +2,23 @@ import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import Header from './components/Header';
 import Navbar from './components/Navbar';
+import Home from './components/Home';
 import RandomActivity from './components/RandomActivity';
 
 
 function App() {
   return (
-    <div className="App">
-      <Router>
+    <Router>
+      <div className="App">
         <Header />
         <Navbar />
 
-        <Route exact path="/" render={(props) => (
-          <div>Home page</div>
-        )}>
-        </Route>
-
-        <Route path="/random" component={RandomActivity}></Route>
-
-      </Router>
-    </div>
+        <div className="container">
+          <Route exact path="/" component={Home}></Route>
+          <Route path="/random" component={RandomActivity}></Route>
+        </div>
+      </div>
+    </Router>
   );
 }
 
