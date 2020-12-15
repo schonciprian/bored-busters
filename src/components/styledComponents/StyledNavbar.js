1f822859f97b192309d5ca28844ede5657c7e981
schonciprian <60438162+schonciprian@users.noreply.github.com>
2020-12-15 12:03:32 +0100

    Styling

--- src/components/styledComponents/StyledNavbar.js
+++ src/components/styledComponents/StyledNavbar.js
@@ -1,5 +1,31 @@
 import styled from "styled-components";
 
 export default styled.div`
-    background-color: blue;
+    height: 40px;
+    padding: 20px;
+
+    font-family: 'Mountains of Christmas';
+    font-size: 30px;
+    font-weight: 700;
+    letter-spacing: 7px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    background-color: #364679;
+
+    a {
+      color: #F7B900;
+      text-decoration: none;
+      padding: 10px;
+    }
+
+    a:hover {
+      color: #FACF3E;
+    }
+
+    @media (max-width: 640px) {
+    font-size: 17px;      
+  }
 `;
