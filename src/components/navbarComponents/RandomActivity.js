02ff7a8c8c85a98ab3fc36203e7dabf1f1b6a98c
schonciprian <60438162+schonciprian@users.noreply.github.com>
2020-12-16 13:47:21 +0100

    Moved favorite button with corresponding methods to a component

--- src/components/navbarComponents/RandomActivity.js
+++ src/components/navbarComponents/RandomActivity.js
@@ -1,31 +1,13 @@
-import React, { useState, useContext } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import StyledGetButton from "../styledComponents/StyledGetButton";
 import StyledActivityContainer from "../styledComponents/StyledActivityContainer";
-import { FavoriteContext } from '../../contextComponents/FavoriteContext';
-import { FaHeart } from 'react-icons/fa';
-import StyledFavButton from '../styledComponents/StyledFavButton';
 import ActivityCardDetails from "../ActivityCardDetails";
+import FavoriteButton from "./searchComponents/FavoriteButton"
 
 
 export default function RandomActivity() {
   const [randomActivity, setrandomActivity] = useState([]);
-  const [favorites, setFavorites] = useContext(FavoriteContext);
-
-  const isCollected = (array) =>
-    array.map((item) => item.activity).includes(randomActivity.activity);
-
-  const removeItem = (array) =>
-    array.filter((element) => element.key !== randomActivity.key);
-
-  const updateFavorites = () => {
-    setFavorites((prevCollection) => {
-      if (!isCollected(prevCollection)) {
-        return [...prevCollection, randomActivity];
-      }
-      return removeItem(prevCollection);
-    });
-  };
 
   const getRandomActivity = () => {
     axios
@@ -41,12 +23,7 @@ export default function RandomActivity() {
         Give me a random activity!
       </StyledGetButton>
 
-      <StyledFavButton style={{ marginRight: "auto", marginLeft: "auto", minHeight: "50px" }} onClick={updateFavorites}>
-        {isCollected(favorites) ?
-          <div style={{ color: "red" }}><FaHeart style={{ height: "40px", width: "40px" }} /></div>
-          : <div><FaHeart style={{ height: "40px", width: "40px" }} /></div>}
-      </StyledFavButton>
-
+      <FavoriteButton activity={randomActivity} />
       <ActivityCardDetails activity={randomActivity} />
 
     </StyledActivityContainer>
@@ -57,7 +34,7 @@ export default function RandomActivity() {
           style={{ marginRight: "auto", marginLeft: "auto", minHeight: "50px" }}
           onClick={getRandomActivity}>
           Give me a random activity!
-      </StyledGetButton>
+        </StyledGetButton>
 
       </StyledActivityContainer>
     );
---
+++ src/components/navbarComponents/searchComponents/FavoriteButton.js
@@ -0,0 +1,31 @@
+import React, { useContext } from "react";
+import { FavoriteContext } from '../../../contextComponents/FavoriteContext';
+import { FaHeart } from 'react-icons/fa';
+import StyledFavButton from '../../styledComponents/StyledFavButton';
+
+export default function FavoriteButton(props) {
+  const [favorites, setFavorites] = useContext(FavoriteContext);
+
+  const isCollected = (array) =>
+    array.map((item) => item.activity).includes(props.activity.activity);
+
+  const removeItem = (array) =>
+    array.filter((element) => element.key !== props.activity.key);
+
+  const updateFavorites = () => {
+    setFavorites((prevCollection) => {
+      if (!isCollected(prevCollection)) {
+        return [...prevCollection, props.activity];
+      }
+      return removeItem(prevCollection);
+    });
+  };
+
+  return (
+    <StyledFavButton style={{ marginRight: "auto", marginLeft: "auto", minHeight: "50px" }} onClick={updateFavorites}>
+      {isCollected(favorites) ?
+        <div style={{ color: "red" }}><FaHeart style={{ height: "40px", width: "40px" }} /></div>
+        : <div><FaHeart style={{ height: "40px", width: "40px" }} /></div>}
+    </StyledFavButton>
+  )
+}
