46eb02f6c9f2a534b18e21ddf7544f07e6517e85
schonciprian <60438162+schonciprian@users.noreply.github.com>
2020-12-16 20:01:23 +0100

    Style: alignment of slider to center added

--- src/components/slidebarComponents/Sliderbar.js
+++ src/components/slidebarComponents/Sliderbar.js
@@ -20,7 +20,7 @@ const SearchByCost = () => {
   };
 
   return (
-    <div style={{ height: 150, width: "100%" }}>
+    <div style={{ height: 150, width: "100%", display: "flex", justifyContent: "center", alignItems: "center" }}>
       <Slider
         mode={2}
         step={1000}
