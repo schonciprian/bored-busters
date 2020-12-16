5512ce86f43c2fe4cd21f485a4e80ae47e989776
schonciprian <60438162+schonciprian@users.noreply.github.com>
2020-12-16 15:20:54 +0100

    Added search functionalities

--- src/components/navbarComponents/Favorites.js
+++ src/components/navbarComponents/Favorites.js
@@ -1,17 +1,55 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { FavoriteContext } from '../../contextComponents/FavoriteContext';
 import ActivityCardDetails from "../ActivityCardDetails";
 import FavoriteButton from "./searchComponents/FavoriteButton"
 import StyledActivityContainer from "../styledComponents/StyledActivityContainer"
+import StyledSelectContainer from "../styledComponents/StyledSelectContainer";
+import StyledInputContainer from "../styledComponents/StyledInputContainer";
+
+import SliderBar from "../slidebarComponents/Sliderbar";
+import { SlideValueContext } from "../../contextComponents/SlideValueContext";
+
 
 export default function Favorites() {
-
+  const [contextValues] = useContext(SlideValueContext);
   const [favorites] = useContext(FavoriteContext);
+
+  const types = ["education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork"]
+  const [activityType, setType] = useState("");
+
+  const [inputParticipants, setparticipants] = useState([]);
+  const clearFields = () => {
+    setparticipants("");
+  };
+
+  console.log(contextValues.min, contextValues.max, activityType, inputParticipants);
+
 
   return favorites.length !== 0 ? (
     <div>
       <div>
-        <h1>helo search things</h1>
+        <StyledSelectContainer>Activity types:
+          <select className="option" onChange={e => setType(e.target.value)}>
+            <option value="">all</option>
+            {types.map((type) => (
+              <option key={type} value={type}>{type}</option>
+            ))}
+          </select>
+        </StyledSelectContainer>
+
+        <StyledInputContainer>
+          Number of participants:
+          <input
+            className="input"
+            type="text"
+            value={inputParticipants}
+            placeholder={inputParticipants}
+            onChange={(e) => setparticipants(e.target.value)}
+            onFocus={clearFields}
+          />
+        </StyledInputContainer>
+
+        <SliderBar />
       </div>
 
       {favorites.map((fav) => (
