b6e2f22074754a0cc49cf08f1738c38871b68b50
schonciprian <60438162+schonciprian@users.noreply.github.com>
2020-12-16 20:42:05 +0100

    Fixing height and font sizes

--- src/components/styledComponents/StyledSelectContainer.js
+++ src/components/styledComponents/StyledSelectContainer.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export default styled.div`
     width: 790px;
-    height: 40px;
+    height: 45px;
     padding: 0;
     margin: 0;
 
@@ -16,17 +16,18 @@ export default styled.div`
     justify-content: center;
 
     background-color: #172251;
+    color: #72CD55;
 
     .option {
         width: auto;
-        height: 50px;
+        height: 40px;
         border: none;
         border-radius: 10px;
         background-color: #253265;
         color: #72CD55;
 
         font-family: 'Mountains of Christmas';
-        font-size: 30px;
+        font-size: 25px;
         font-weight: 900;
         letter-spacing: 7px;
         text-align: center;
