6329ce81cd1097b1b56a0c0af4f1cff94cae13ed
schonciprian <60438162+schonciprian@users.noreply.github.com>
2020-12-16 14:06:04 +0100

    Removed commented, unnecessary lines

--- src/components/navbarComponents/Favorites.js
+++ src/components/navbarComponents/Favorites.js
@@ -1,8 +1,5 @@
 import React, { useContext } from 'react';
 import { FavoriteContext } from '../../contextComponents/FavoriteContext';
-// import StyledFavCards from '../styledComponents/StyledFavCards';
-// import { FaHeart } from 'react-icons/fa';
-// import StyledFavButton from '../styledComponents/StyledFavButton';
 import ActivityCardDetails from "../ActivityCardDetails";
 import FavoriteButton from "./searchComponents/FavoriteButton"
 import StyledActivityContainer from "../styledComponents/StyledActivityContainer"
@@ -17,30 +14,6 @@ export default function Favorites() {
         <FavoriteButton activity={fav} />
         <ActivityCardDetails activity={fav} />
       </React.Fragment>
-    </StyledActivityContainer>
-
-    // <StyledFavCards>
-
-    //   <div style={{ fontSize: "35px", height: "100px" }}>{fav.activity}</div>
-
-    //   <div>Type: {fav.type}</div>
-
-    //   {fav.link ? (
-    //     <div>
-    //       Visit: <a href={fav.link}> {fav.link}</a>{" "}
-    //     </div>
-    //   ) : ("")}
-
-    //   <div>Number of participants:{fav.participants}</div>
-
-    //   <div>Price: {fav.price * 10000}</div>
-
-    //   <StyledFavButton style={{ marginRight: "auto", marginLeft: "auto", minHeight: "50px" }} >
-    //     <div><FaHeart style={{ height: "40px", width: "40px" }} /></div>
-    //   </StyledFavButton>
-    // </StyledFavCards>
-
-
-  )
+    </StyledActivityContainer>)
   )
 }
