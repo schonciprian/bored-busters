33d22890cb47501f5c7e2df84daef35f384ba3d6
schonciprian <60438162+schonciprian@users.noreply.github.com>
2020-12-16 14:05:45 +0100

    Change context to components

--- src/components/navbarComponents/Favorites.js
+++ src/components/navbarComponents/Favorites.js
@@ -1,35 +1,44 @@
 import React, { useContext } from 'react';
 import { FavoriteContext } from '../../contextComponents/FavoriteContext';
-import StyledFavCards from '../styledComponents/StyledFavCards';
-import { FaHeart } from 'react-icons/fa';
-import StyledFavButton from '../styledComponents/StyledFavButton';
+// import StyledFavCards from '../styledComponents/StyledFavCards';
+// import { FaHeart } from 'react-icons/fa';
+// import StyledFavButton from '../styledComponents/StyledFavButton';
+import ActivityCardDetails from "../ActivityCardDetails";
+import FavoriteButton from "./searchComponents/FavoriteButton"
+import StyledActivityContainer from "../styledComponents/StyledActivityContainer"
 
 export default function Favorites() {
 
-  const [favorites, setFavorites] = useContext(FavoriteContext);
+  const [favorites] = useContext(FavoriteContext);
 
   return favorites.map((fav) => (
+    <StyledActivityContainer style={{ width: "650px" }}>
+      <React.Fragment>
+        <FavoriteButton activity={fav} />
+        <ActivityCardDetails activity={fav} />
+      </React.Fragment>
+    </StyledActivityContainer>
 
-    <StyledFavCards>
+    // <StyledFavCards>
 
-      <div style={{ fontSize: "35px", height: "100px" }}>{fav.activity}</div>
+    //   <div style={{ fontSize: "35px", height: "100px" }}>{fav.activity}</div>
 
-      <div>Type: {fav.type}</div>
+    //   <div>Type: {fav.type}</div>
 
-      {fav.link ? (
-        <div>
-          Visit: <a href={fav.link}> {fav.link}</a>{" "}
-        </div>
-      ) : ("")}
+    //   {fav.link ? (
+    //     <div>
+    //       Visit: <a href={fav.link}> {fav.link}</a>{" "}
+    //     </div>
+    //   ) : ("")}
 
-      <div>Number of participants:{fav.participants}</div>
+    //   <div>Number of participants:{fav.participants}</div>
 
-      <div>Price: {fav.price * 10000}</div>
+    //   <div>Price: {fav.price * 10000}</div>
 
-      <StyledFavButton style={{ marginRight: "auto", marginLeft: "auto", minHeight: "50px" }} >
-        <div><FaHeart style={{ height: "40px", width: "40px" }} /></div>
-      </StyledFavButton>
-    </StyledFavCards>
+    //   <StyledFavButton style={{ marginRight: "auto", marginLeft: "auto", minHeight: "50px" }} >
+    //     <div><FaHeart style={{ height: "40px", width: "40px" }} /></div>
+    //   </StyledFavButton>
+    // </StyledFavCards>
 
 
   )
