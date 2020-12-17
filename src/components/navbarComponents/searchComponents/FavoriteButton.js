93c318614d5b06f7d1b2be8f310953597d28ae0a
schonciprian <60438162+schonciprian@users.noreply.github.com>
2020-12-17 14:24:10 +0100

    Bugfix with pagination: when removing a favorite item, it jumps back one page

--- src/components/navbarComponents/searchComponents/FavoriteButton.js
+++ src/components/navbarComponents/searchComponents/FavoriteButton.js
@@ -17,7 +17,7 @@ export default function FavoriteButton(props) {
       if (!isCollected(prevCollection)) {
         return [...prevCollection, props.activity];
       }
-      if (props.setCurrentPageOriginals) {
+      if (props.setCurrentPageOriginals && props.currentPageOriginals > 1 && props.currentPageOriginals.length === 0) {
         props.setCurrentPageOriginals(props.currentPageOriginals - 1);
       }
       return removeItem(prevCollection);
